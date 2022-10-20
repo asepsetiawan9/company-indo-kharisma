f4d0a658b586895ec818fbd280f6151f05d1f215
asepsetiawan9 <zep.setiawan09@gmail.com>
2022-10-20 16:19:32 +0700

    add header, background+responsive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import './assets/css/Styles.css'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,40 +1,67 @@
 import React from 'react';
 import {Container, Row, Col, Dropdown} from 'react-bootstrap';
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiInfo, FiList, FiPackage, FiUsers, FiPhone } from "react-icons/fi";
 import logo from '../assets/images/logo.png'
 function Index() {
   return (
     <Container fluid>
-      <Row>
-        <Col className='d-flex' style={{background: 'yellow', justifyContent: 'space-between'}}  lg={4} >
-          <div >
-            <img style={{width: '50px'}} src={logo} alt='Logo'></img>
+      <Row style={{paddingLeft: '100px', paddingRight: '100px'}}>
+        <Col className='header' md={4} >
+          <div>
+            <img style={{width: '80px'}} src={logo} alt='Logo'></img>
           </div>
           <div>
-            <Dropdown className="d-sm-flex flex-row d-lg-none" >
-              <Dropdown.Toggle  id="dropdown-basic" style={{background: 'green', justifyContent: 'center', alignItems: 'center'}}>
+            <Dropdown className="d-sm-flex flex-row d-md-none" >
+              <Dropdown.Toggle style={{color: 'black'}} id="dropdown-basic" className='dropdownTogle'>
                 <FiMenu/>
               </Dropdown.Toggle>
-
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <div className='d-flex gap-5' style={{padding:'20px'}}>
+                  <FiInfo size={30}/>
+                  <FiList size={30}/>
+                  <FiPackage size={30}/>
+                  <FiUsers size={30}/>
+                  <FiPhone size={30}/>
+                </div>
               </Dropdown.Menu>
             </Dropdown>
           </div>
-        
         </Col>
-
-        <Col className="d-lg-flex flex-row d-none" style={{background: 'red'}} sm={12} lg={8}>
-          <div>menu 1</div>
-          <div>menu 2</div>
-          <div>menu 3</div>
-          <div>menu 4</div>
+        <Col className="d-md-flex flex-row d-none gap-4 mainMenu" sm={12} md={8}>
+          <div>About</div>
+          <div>Feature</div>
+          <div>Product</div>
+          <div>Team</div>
+          <div>Contact</div>
         </Col>
-
-        
       </Row>
+      <section>
+        <Row>
+          <div className="bg d-flex" style={{justifyContent: 'center', alignItems: 'center', paddingLeft: '100px', paddingRight: '100px'}}>
+            <Col md={6} >
+              <div style={{fontSize: '30px', fontWeight: '500'}}>
+                Supplying across the globe
+              </div>
+              <div style={{fontSize: '40px', fontWeight: '500'}}>
+                The Leading Supplier of Coconut Product
+              </div>
+            </Col>
+            <Col md={6} >
+            </Col>
+          </div>
+        </Row>
+      </section>
+
+      <section>
+        <Row>
+          <Col md={6} sm={12}>
+            lorem
+          </Col>
+          <Col md={6} sm={12}>
+            lorem
+          </Col>
+        </Row>
+      </section>
     </Container>
   );
 }
