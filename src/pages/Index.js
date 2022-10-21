4e0e35cc4b6f553e7e779da4daa68c9f854deaa4
asepsetiawan9 <zep.setiawan09@gmail.com>
2022-10-21 09:40:20 +0700

    add about and feature company

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Container, Row, Col, Dropdown} from 'react-bootstrap';
 import { FiMenu, FiInfo, FiList, FiPackage, FiUsers, FiPhone } from "react-icons/fi";
+import { FaAward, FaRegHandPeace, FaHandshake, FaLightbulb } from "react-icons/fa";
 import logo from '../assets/images/logo.png'
+import imgabout from '../assets/images/imgabout.png'
 function Index() {
   return (
     <Container fluid>
@@ -54,11 +56,66 @@ function Index() {
 
       <section>
         <Row>
-          <Col md={6} sm={12}>
-            lorem
+          <Col md={6} sm={12} style={{padding: '100px 100px'}}>
+            <div style={{fontSize: '35px', fontWeight: '500'}}>About Us</div>
+            <div style={{fontSize: '22px', padding: '20px 0px'}}>Universal Coco Indonesia is the leading supplier of coconut products from Indonesia. We are a company engaged in manufacturing various coconut products with high-quality materials. We process more than 1,500 hectares of coconut plantations, spread across several islands in Eastern Indonesia.</div>
+            <div style={{padding: '20px 0px'}}>In the beginning, our company was engaged in manufacturing where we processed coconut derivative products, namely coconut shells into coconut shell charcoal. And after that our company expanded and expanded into coconut plantations, to process more derivative products.</div>
+            <div>Universal Coco Indonesia has served customers from all over the world and it has become our top priority to meet our customers’ satisfaction and expectations.</div>
           </Col>
-          <Col md={6} sm={12}>
-            lorem
+          <Col md={6} sm={12} style={{paddingTop: '100px', alignItems: 'center'}}>
+          <img  src={imgabout} alt='Logo'></img>
+          </Col>
+        </Row>
+      </section>
+
+      <section>
+        <Row style={{padding: '0px 100px'}}>
+          <Col md={12} style={{fontSize: '35px', fontWeight: '500', textAlign: 'center', paddingBottom: '50px'}}>
+            Our Key Features
+          </Col>
+          <Col md={6} sm={12} className='d-flex flex-column gap-3' style={{padding: '24px'}}>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <FaRegHandPeace size={50}/>
+            </div>
+            
+            <div>
+              Integrity
+            </div>
+            <div>
+              Integrity means telling the truth, keeping our word, and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.
+            </div>
+          </Col>
+          <Col md={6} sm={12} className='d-flex flex-column gap-3' style={{padding: '24px'}}>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <FaAward size={50}/>
+            </div>
+
+            <div>Quality</div>
+            <div>
+              Quality is exhibited in many ways by selling and supporting products and services that delight customers, establishing a work environment, and delivering financial results that meet investor expectations.
+            </div>
+          </Col>
+          <Col md={6} sm={12} className='d-flex flex-column gap-3' style={{padding: '24px'}}>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <FaLightbulb size={50}/>
+            </div>
+            
+            <div>
+              Innovation
+            </div>
+            <div>
+              Innovation means inventing, designing, and developing products and services that have high appeal in the marketplace and strengthen customer preference for the Universal Coco Indonesia project.
+            </div>
+          </Col>
+          <Col md={6} sm={12} className='d-flex flex-column gap-3' style={{padding: '24px'}}>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <FaHandshake size={50}/>
+            </div>
+            
+            <div>Commitment</div>
+            <div>
+              Commitment is important where we will continue to provide the best to our customers and investors all the time. Our opportunity to serve should be viewed as a privilege that is not to be taken for granted.
+            </div>
           </Col>
         </Row>
       </section>
